7c3ec632035ce0f36f2dbccb81a46e891d30d093
wenhaoqi <wenhaoqi@126.com>
2021-02-10 21:43:16 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     'gatsby-plugin-netlify-cms',
-    
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
