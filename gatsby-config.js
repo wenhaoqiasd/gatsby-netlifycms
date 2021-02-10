be87f8fe3728b1a1efc218c85a5cd9717ea30114
wenhaoqi <wenhaoqi@126.com>
2021-02-10 18:16:15 +0800

    up

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,8 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    'gatsby-plugin-netlify-cms',
+    
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
