4da5ff56d84f332f148a855618982c011d731947
wenhaoqi <wenhaoqi@126.com>
2021-02-17 14:51:33 +0800

    --

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,3 @@
 import CMS from "netlify-cms-app"
-import styles from '!css-loader!./style.css';
+import styles from '!css-loader!./cms.css';
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
