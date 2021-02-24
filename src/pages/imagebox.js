375484da3447398fad9c5f8b916ccbb6adb27a29
wenhaoqi <wenhaoqi@126.com>
2021-02-24 23:47:13 +0800

    更新++

---
+++ src/pages/imagebox.js
@@ -0,0 +1,114 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Footer from "../components/footer"
+
+import Clipboard from 'react-clipboard.js'
+
+const boxStyles = {
+  position: "relative",
+  padding: "0 0 4rem",
+}
+
+const imageBoxStyle = {
+  width: "100%",
+  overflow: "auto",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-end",
+  padding: "2rem 1.5rem 0",
+  boxShadow: "0 1px 0 var(--Text-3)",
+}
+
+const coverStyle = {
+  width: "20rem",
+  maxWidth: "64vw",
+  marginRight: "1.5rem",
+  boxShadow: "0 0.5rem 1rem 0 rgba(0, 0, 0, 0.08)",
+}
+
+const copylinkStyle = {
+  outline: "none",
+  border: "0",
+  cursor: "pointer",
+  margin: "0 -2rem 0 0",
+  boxShadow: "0 0 0 1px var(--Border)",
+}
+
+const docimageStyle = {
+  width: "15rem",
+  display: "block",
+  flex: "0 0 auto",
+}
+
+const textStyle = {
+  padding: "0 1.5rem",
+  width: "100%",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+}
+
+const textTitle = {
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  fontWeight: "600",
+}
+
+const ImageBox = () => {
+
+  function copyLinkAlert() {
+    alert("Done.");
+  }
+
+  const data = useStaticQuery(graphql`
+    query ImageQuery {
+      resources: allAirtable(filter: {table: {eq: "Resources"}}) {
+        nodes {
+          data {
+            Blog
+            Document {
+              url
+            }
+            Link
+            Cover {
+              url
+            }
+            Video {
+              url
+            }
+          }
+          recordId
+        }
+      }
+    }  
+  `);
+  return (
+    <Layout>
+      <SEO title="ImageBox" />
+      <main className="zoom-out" style={{ padding: "4.5rem 0 0" }}>
+        {data.resources.nodes.map((item) => (
+          <div key={item.recordId} style={boxStyles}>
+            <div style={imageBoxStyle}>
+              <img style={coverStyle} src={item.data.Cover[0].url} alt={item.data.Blog} />
+              {item.data.Document.map((doc) => (
+                <Clipboard onClick={copyLinkAlert} style={copylinkStyle} data-clipboard-text={doc.url} key={doc.url}>
+                  <img style={docimageStyle} src={doc.url} alt={doc.url} />
+                </Clipboard>
+              ))}
+            </div>
+            <div style={textStyle}>
+              <h4 style={textTitle}>{item.data.Blog}</h4>
+              <Link className="action" to={item.data.Link}>wenhaoqi.com{item.data.Link}</Link>
+            </div>
+          </div>
+        ))}
+        <Footer />
+      </main>
+    </Layout>
+  )
+}
+export default ImageBox
