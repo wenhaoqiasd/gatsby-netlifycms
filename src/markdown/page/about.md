cdced0608884c37dff6b341e5a987795b0322046
wenhaoqi <wenhaoqi@126.com>
2021-08-02 01:33:08 +0800

    update

--- src/markdown/page/about.md
+++ src/markdown/page/about.md
@@ -8,6 +8,16 @@ title: About
 
 ![](/assets/me.png)
 
-- - -
+#### My name is Curiosity Wen, I’m Chinese he/him living in Shanghai, China. Design is the medium through which I express myself.
+#### I’m currently the product designer for aDrive at Alibaba. I’ve and also work on building design system. Before that, I worked on Teambition Thoughts, 100offer, and AMILY. I love building tools that people depend on for their work.
 
-#### I'm Curiosity Wen, product designer of Alibaba AliDrive team. Used to work in ecommerce, recruitmentindustry, engaged in digital product design. And also work in other fields such as Motion Graphic, Illustration.
+<!-- <a href="ha.html" target="_blank" rel="noopener noreferrer"><h4>Read about my work →</h4></a> -->
+
+---
+
+#### This website
+
+This site was built by Gatsby. Articles are written using Markdown.
+It's hosted by Github and distributed globally by Netlify.
+
+It‘s my playground for learning design and front-end technologies.
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,22 +25,17 @@ const isBrowser = () => typeof window !== "undefined"
 
 async function loadFont() {
   if (!isBrowser()) return
-
   const fontUrl = `url(${window.location.href.split("#")[0]}fonts/WidescreenVF-WghtWdthMixd.ttf)`
   const font = new FontFace("WidescreenVF", fontUrl);
   await font.load()
-
   document.fonts.add(font)
   // document.body.classList.add('fonts-loaded')
 }
-
 loadFont()
 
-
-
 const IndexPage = () => {
-  const [fontLoaded, setFontLoaded] = useState(false)
 
+  const [fontLoaded, setFontLoaded] = useState(false)
   const pageName = "Home"
 
   useEffect(() => {
@@ -63,8 +58,8 @@ const IndexPage = () => {
                   <span className="big-motion-1">
                     <RandomReveal
                       isPlaying
-                      duration={2}
-                      revealDuration={2}
+                      duration={4}
+                      revealDuration={4}
                       characters="DESIGN"
                       characterSet="DEVELOPCREATE"
                       onComplete={() => [false, 12000]}
@@ -100,10 +95,13 @@ const IndexPage = () => {
           </section>
 
           <section className="home-04">
-            <h2>Say hi!</h2>
+            <h4>My name is Curiosity Wen, I’m Chinese he/him living in Shanghai, China. Design is the medium through which I express myself.</h4>
             <h4>
-              I'm Curiosity Wen, product designer of Alibaba <span className="hover-img"><img src="/assets/a.svg" alt="" />aDrive team.</span>&nbsp;Used to work in ecommerce, recruitmentindustry, engaged in digital product design. And also work in other fields such as Motion Graphic, Illustration_
+              I’m currently the product designer for <span className="hover-img"><img src="/assets/a.svg" alt="" />aDrive</span> at Alibaba.&nbsp;
+              I’ve and also work on building design system. Before that, I worked on Teambition Thoughts, 100offer,&nbsp;
+              and AMILY. I love building tools that people depend on for their work.
             </h4>
+            <a href="https://www.figma.com/community/plugin/986289377230504703/DarkSide" target="_blank" rel="noopener noreferrer" className="block-link"><h4>Figma plug-in DarkSide →</h4></a>
             <Grid GridType="mobile-type-t type-321" />
           </section>
         </div>
