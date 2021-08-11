872e318adeab0c66b5ede42e0f79b17a7920fb71
温皓淇 <whq209442@alibaba-inc.com>
2021-08-11 19:11:45 +0800

    视差组件抽象api雏形设计

--- src/components/parallaxbox.js
+++ src/components/parallaxbox.js
@@ -1,16 +1,31 @@
 import React, { useRef, useState, useEffect } from 'react'
 import {MyContext} from '../context/mycontext'
 
+const ParallaxStyle = {
+  position: "relative",
+  width: "100%",
+  minHeight: "100vh",
+  overflow: "hidden"
+}
+
 const ParallaxBox = (props) => {
 
   const First = useRef(0)
 
   const [parallax, setParallax] = useState(100)
   const scrollHandler = () => {
+    // 可视高
     const clientH = window.innerHeight || document.documentElement.clientHeight
+    // 容器距离顶部距离
     const scrollTop = First.current.getBoundingClientRect().top
+    // 容器高度
+    const scrollH = First.current.getBoundingClientRect().height
+    // 元素滚动百分比
     const top100 = 100 - (scrollTop / clientH * 100)
-    setParallax(top100)
+    // 元素高度百分比
+    const height100 = (scrollH / clientH * 100)
+    // 出画停止计算
+    top100 < (height100 + 100) ? setParallax(top100) : setParallax(height100 + 100)
   }
 
   useEffect(() => {
@@ -21,7 +36,7 @@ const ParallaxBox = (props) => {
   }, []);
 
   return (
-    <div ref={First}>
+    <div ref={First} style={ParallaxStyle} className={props.className}>
       <MyContext.Provider value={{value: parallax}}>
         {props.children}
       </MyContext.Provider>
--- src/pages/parallaxpage.js
+++ src/pages/parallaxpage.js
@@ -1,38 +1,62 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import PropTypes from "prop-types"
 
 import ParallaxBox from '../components/parallaxbox'
-import {MyContext} from '../context/mycontext'
+import { MyContext } from '../context/mycontext'
 
 import "./parallax.css"
 
-const Section1 = () => {
+
+const Section = ({ X, Y, TranslateX, TranslateY, DirectionX, DirectionY, Fade, DirectionF, children, className, Speed }) => {
+  // 滚动数据给子元素
   const para = useContext(MyContext)
+  // X
+  const direction_x = DirectionX ? (X + para.value) * Speed : (100 - X - para.value) * Speed
+  const numX = direction_x
+  // Y
+  const direction_y = DirectionY ? (Y + para.value) * Speed : (100 - Y - para.value) * Speed
+  const numY = direction_y
+  // 变换样式
+  const styles = (TranslateX ? 'translateX(' + numX + 'vw)' : '') + (TranslateY ? 'translateY(' + numY + 'vh)' : '') + 'translateZ(1px)'
+  // Fade
+  const direction_f = (DirectionF ? (para.value / 100 - 1) : (2 - para.value / 100))
+  const fade = (Fade ? direction_f : '')
+  const cName = className
   return (
-    <div className="parallaxbox">
-      <h1 style={{transform: 'translateX(' + (para.value > 100 ? para.value - 100 : 0) + 'px)'}}>First {para.value.toFixed(2)}%</h1>
+    <div style={{ top: 128, transform: styles, opacity: fade }} className={cName} >
+      {children}
     </div>
   )
 }
 
-const Section2 = () => {
-  const para = useContext(MyContext)
-  return (
-    <div className="parallaxbox">
-      <h1 style={{transform: 'translateX(' + (para.value > 100 ? para.value - 100 : 0) + 'px)'}}>Second {para.value.toFixed(2)}%</h1>
-    </div>
-  )
+Section.propTypes = {
+  X: PropTypes.number,
+  Y: PropTypes.number,
+  TranslateX: PropTypes.bool,
+  TranslateY: PropTypes.bool,
+  DirectionX: PropTypes.bool,
+  DirectionY: PropTypes.bool,
+  Fade: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  DirectionF: PropTypes.bool,
+  className: PropTypes.string,
+  Speed: PropTypes.number
+}
+Section.defaultProps = {
+  X: 0,
+  Y: 0,
+  TranslateX: true,
+  TranslateY: false,
+  DirectionX: true,
+  DirectionY: true,
+  Fade: true,
+  DirectionF: true,
+  className: '',
+  Speed: 1
 }
 
-const Section3 = () => {
-  const para = useContext(MyContext)
-  return (
-    <div className="parallaxbox">
-      <h1 style={{transform: 'translateX(' + (para.value > 100 ? para.value - 100 : 0) + 'px)'}}>Third {para.value.toFixed(2)}%</h1>
-    </div>
-  )
-}
 
 
 const ParallaxPage = () => {
@@ -41,13 +65,55 @@ const ParallaxPage = () => {
     <Layout>
       <Seo title="ParallaxPage" />
       <ParallaxBox>
-        <Section1 />
+        <Section
+          X={0}
+          Y={0}
+          TranslateX={true}
+          TranslateY={true}
+          DirectionX={true}
+          DirectionY={true}
+          Fade={false}
+          DirectionF={true}
+          Speed={0.8}
+        >
+          <div style={{ width: 300, height: 300, backgroundColor: 'red' }}>
+            Card
+          </div>
+        </Section>
+      </ParallaxBox>
+      <ParallaxBox className="Popo">
+        <Section
+          X={0}
+          Y={0}
+          TranslateX={true}
+          TranslateY={true}
+          DirectionX={true}
+          DirectionY={true}
+          Fade={false}
+          DirectionF={true}
+          Speed={0.8}
+        >
+          <div style={{ width: 300, height: 300, backgroundColor: 'red' }}>
+            Card
+          </div>
+        </Section>
       </ParallaxBox>
       <ParallaxBox>
-        <Section2 />
-      </ParallaxBox>
-      <ParallaxBox>
-        <Section3 />
+        <Section
+          X={0}
+          Y={0}
+          TranslateX={true}
+          TranslateY={true}
+          DirectionX={true}
+          DirectionY={true}
+          Fade={false}
+          DirectionF={true}
+          Speed={0.8}
+        >
+          <div style={{ width: 300, height: 300, backgroundColor: 'red' }}>
+            Card
+          </div>
+        </Section>
       </ParallaxBox>
     </Layout>
   )
